e5073e9728725637f3df7da3e176c781b6cc1b36
Shaun Lawrie <shaun.r.lawrie@gmail.com>
2024-04-18 19:40:56 +1200

    Re-init repo

---
+++ README.md
@@ -0,0 +1,4 @@
+![alt text](/public/image.png)
+
+This is just the source for my website [shaunlawrie.com](https://shaunlawrie.com/).  
+It uses [Astro](https://astro.build/) because it's the only web framework that I've found hassle-free.  
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Shaun Lawrie';
+export const SITE_DESCRIPTION = 'A collection of stuff I\'ve made.';
+export const TWITTER_HANDLE = '@shaun_lawrie';
